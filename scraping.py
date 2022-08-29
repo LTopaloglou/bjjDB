bdce3dc243f6eddc247aa7967b3433b051ebde61
Leo <leo.topalogloumundy@gmail.com>
2022-08-28 22:24:18 -0400

    athlete data scraped & printed to console

--- scraping.py
+++ scraping.py
@@ -1,3 +1,5 @@
+from tkinter import N
+import queries
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,10 +12,38 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="tablepress-104")
 
-firstNames = results.find_all("td", class_="column-1")
-lastNames = results.find_all("td", class_="column-2")
-nickNames = results.find_all("td", class_="column-3")
-teams = results.find_all("td", class_="column-4")
+#SQL queries
+connection = queries.create_database_connection("localhost", "root", queries.pw, "bjj")
+#this query creates the athletes table with all the columns.
+create_table_query = ("CREATE TABLE athletes("
+                      "id int PRIMARY KEY,"
+                      "First_Name text,"
+                      "Last_Name text,"
+                      "Nickname text,"
+                      "Team text)")
 
-for firstName in firstNames:
-    print(firstName.find("a").string)
+
+row = 2
+rowData = results.find(class_="row-"+str(row))
+print(rowData)
+while rowData is not None:
+    print("row-"+str(row))
+    firstName = rowData.find(class_="column-1").find("a").string
+    print(firstName)
+    lastName = rowData.find(class_="column-2").find("a").string
+    print(lastName)
+    nickName = rowData.find(class_="column-3")
+    if nickName is not None:
+        print(nickName.string)
+    else:
+        print("ERROR: EMPTY DATA")
+    team = rowData.find(class_="column-4")
+    if team is not None:
+        print(team.string)
+    else:
+        print("ERROR: EMPTY DATA")
+    link = "https://www.bjjheroes.com" + rowData.find(class_="column-1").find("a")['href']
+    print(link)
+    print()
+    row += 1
+    rowData = results.find(class_="row-"+str(row))
